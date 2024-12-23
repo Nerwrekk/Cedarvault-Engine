37456c9499bce2f88abd688b13420b25c4b6018a
Nerwrekk <alexander.tanaskovic@hotmail.se>
2024-12-23 16:44:20 +0100

    Started working on SDL_Events

--- Engine/include/Application.h
+++ Engine/include/Application.h
@@ -1,5 +1,8 @@
 #pragma once
 
+class SDL_Window;
+class SDL_Renderer;
+
 namespace cedar
 {
 	class Application
@@ -15,6 +18,11 @@ namespace cedar
 		void ProccessInput();
 		void Update();
 		void Render();
+
+	private:
+		SDL_Window* m_window;
+		SDL_Renderer* m_renderer;
+		bool IsRunning;
 	};
 
 } // namespace cedar
--- Engine/src/Application.cpp
+++ Engine/src/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include <SDL2/SDL.h>
+#include <iostream>
+
 namespace cedar
 {
 	Application::Application()
@@ -14,6 +17,31 @@ namespace cedar
 
 	void Application::Initialize()
 	{
+		if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+		{
+			std::cerr << "Error init SDL" << std::endl;
+
+			return;
+		}
+
+		m_window = SDL_CreateWindow(
+		    "Game",
+		    SDL_WINDOWPOS_CENTERED,
+		    SDL_WINDOWPOS_CENTERED,
+		    800,
+		    600,
+		    SDL_WINDOW_BORDERLESS);
+
+		m_renderer = SDL_CreateRenderer(
+		    m_window,
+		    -1, //Get default one
+		    0   //no flags
+		);
+
+		if (!m_renderer)
+		{
+			std::cerr << "Error creating SDL renderer" << std::endl;
+		}
 	}
 
 	void Application::Run()
@@ -28,10 +56,21 @@ namespace cedar
 
 	void Application::Destroy()
 	{
+		SDL_DestroyWindow(m_window);
+		SDL_DestroyRenderer(m_renderer);
+
+		SDL_Quit();
 	}
 
 	void Application::ProccessInput()
 	{
+		SDL_Event sdlEvent;
+		while (SDL_PollEvent(&sdlEvent))
+		{
+			switch (sdlEvent.type)
+			{
+			}
+		}
 	}
 
 	void Application::Update()
