31382398f44e45c530519c9c8d7552df4025859f
Nerwrekk <alexander.tanaskovic@hotmail.se>
2024-12-23 20:42:58 +0100

    Added assets folder and manage to draw tank on the screen

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,8 @@ class MeanPixelRecipe(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
-        self.requires("sdl/2.30.8")
+        self.requires("sdl/2.28.3")
+        self.requires("sdl_image/2.6.3")
         self.requires("sol2/3.3.1")
         self.requires("glm/cci.20230113")
 
