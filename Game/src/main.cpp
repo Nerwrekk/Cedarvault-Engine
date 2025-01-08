3b60af9d3c588747a505dccbf8dddeeef4fad6b1
Nerwrekk <alexander.tanaskovic@hotmail.se>
2025-01-08 01:10:21 +0100

    Added truck entity

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -10,7 +10,12 @@ int main()
 		auto tank = app.Manager()->CreateEntity();
 		tank.AddComponent<cedar::RigidBodyComponent>(glm::vec2(50.f, 0.f));
 		tank.AddComponent<cedar::SpriteComponent>("./assets/images/tank-panther-right.png", 32, 32);
-	});
+
+		auto truck = app.Manager()->CreateEntity();
+		truck.AddComponent<cedar::RigidBodyComponent>(glm::vec2(30.f, -10.f));
+		truck.AddComponent<cedar::SpriteComponent>("./assets/images/truck-ford-right.png", 32, 32);
+		truck.GetComponent<cedar::TransformComponent>()->Position = glm::vec2 { 400, 500 };
+		});
 
 	app.Run();
 
